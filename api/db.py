32e7c70bbd7704db041c7945ca9089e66543bdef
7s <reinyu813@gmail.com>
2026-05-15 10:43:24 +0900

    Add import for importlib.util in db.py

--- api/db.py
+++ api/db.py
@@ -7,6 +7,7 @@ The translator remains usable without MongoDB. Callers should catch
 
 from datetime import datetime, timezone
 import importlib
+import importlib.util
 import os
 from urllib.parse import urlparse
 
